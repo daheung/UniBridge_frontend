5a9b6bc064a4ce13625bd4c980a9b4f5e09d5ddc
sherry990105 <sherry99015@gmail.com>
2026-03-10 04:18:14 +0900

    260310 04:17 김재민 검색목록, 멘토링 js 파일 추가

--- frontend/assets/js/user/mentorSearch/mentorSearch.js
+++
@@ -1,7 +0,0 @@
-const matching = document.getElementById("matching");
-
-matching.addEventListener("click", () => {
-  
-  location.href = "/html/user/mentorSearch/mentorDetail/MentorDetail.html";
-})
-
